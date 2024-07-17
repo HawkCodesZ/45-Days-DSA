cc63971bf3674edfc600c2ad8645ceeeda700095
HawkCodesZ <Amayamix24@gmail.com>
2024-07-17 20:03:54 +0530

    second commit

---
+++ 75-SortColors.py
@@ -0,0 +1,20 @@
+class Solution:
+    def sortColors(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        for i in range(n):
+            swapped = False
+
+            # Last i elements are already in place
+            for j in range(0, n-i-1):
+
+                # Traverse the array from 0 to n-i-1
+                # Swap if the element found is greater
+                # than the next element
+                if nums[j] > nums[j+1]:
+                    nums[j], nums[j+1] = nums[j+1], nums[j]
+                    swapped = True
+            if (swapped == False):
+                break
